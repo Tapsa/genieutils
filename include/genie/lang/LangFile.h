cf5b3519bc9e8df112c56f2905a65c05fa923766
Mikko <1067314+Tapsa@users.noreply.github.com>
2021-07-21 10:59:19 +0300

    Restore Windows compatibility

--- include/genie/lang/LangFile.h
+++ include/genie/lang/LangFile.h
@@ -28,15 +28,15 @@ struct pcr_file;
 
 namespace genie
 {
-  
+
 class PcrioError : public std::ios::failure
 {
 public:
   explicit PcrioError(int error);
   int getError(void) const { return error_; }
-  
+
   static void check(int error);
-  
+
 private:
   int error_;
 };
@@ -50,54 +50,54 @@ public:
 /// TODO Proper error handling
 class LangFile : public IFile
 {
-  
+
 public:
   LangFile();
   virtual ~LangFile();
-  
+
   virtual void load(const char *fileName);
   virtual void saveAs(const char *fileName);
-  
+
   // get/set strings in default_charset (utf-8)
   std::string getString(unsigned int id);
   void setString(unsigned int id, std::string str);
-  
+
   /// Change the default charset. See libiconv doc for available ones.
   /// If not set, default = UTF8.
   void setDefaultCharset(const char *charset);
-  
+
 protected:
-  
+
   virtual void unload(void);
-  
+
   virtual void serializeObject(void) {}
-  
+
 private:
-  
+
   static Logger &log;
-  
+
   struct pcr_file *pfile_;
-  
+
   uint32_t defaultCultureId_;
   uint32_t defaultCodepage_;
-  
+
   static const unsigned int CONV_BUF_SIZE = 100;
   static const char *CONV_DEFAULT_CHARSET;
-  
+
   std::string systemDefaultCharset_;  // all strings will be converted from/to this charset
-  
-  iconv_t toDefaultCharsetCd_; 
-  iconv_t fromDefaultCharsetCd_; 
-  
+
+  iconv_t toDefaultCharsetCd_;
+  iconv_t fromDefaultCharsetCd_;
+
   /// Convert a utf8 string to codepage
   std::string convertTo(std::string in, uint32_t codepage);
-  
+
   /// Convert a string from codepage to utf8
   std::string convertFrom(std::string in, uint32_t codepage);
-  
+
   std::string convert(iconv_t cd, std::string input);//char *in_ptr, size_t in_size);
 };
-  
+
 }
 
 #endif // GENIE_LANGFILE_H
